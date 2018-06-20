e0932ab8e8356783fa305116511b4f74c5aed545
cruxbreaker <geekankit318@gmail.com>
2018-06-20 08:21:37 +0530

    Added Last Converted details and Conversion Rate

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -5,11 +5,14 @@ import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
 import { ClearButton } from '../components/Button';
+import { LastConverted } from '../components/Text';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'INR';
-const TEMP_BASE_PRICE = '1';
-const TEMP_QUOTE_PRICE = '68.04';
+const TEMP_BASE_PRICE = '10';
+const TEMP_QUOTE_PRICE = '680.4';
+const TEMP_CONVERSION_RATE = 68.04;
+const TEMP_CONVERSION_DATE = new Date();
 
 class Home extends Component {
 
@@ -47,6 +50,12 @@ class Home extends Component {
                 editable={false}
                 defaultValue={TEMP_QUOTE_PRICE}
 
+            />
+            <LastConverted 
+                base={TEMP_BASE_CURRENCY}
+                quote={TEMP_QUOTE_CURRENCY}
+                conversionRate={TEMP_CONVERSION_RATE}
+                date={TEMP_CONVERSION_DATE}
             />
             <ClearButton 
                 text="Reverse Currencies"
