e53ca38e514511dbee805da6318bc0a15dc5a80d
cruxbreaker <geekankit318@gmail.com>
2018-06-20 08:45:36 +0530

    Added Home Screen: Header Option

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -6,6 +6,7 @@ import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
 import { ClearButton } from '../components/Button';
 import { LastConverted } from '../components/Text';
+import { Header } from '../components/Header';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'INR';
@@ -25,17 +26,24 @@ class Home extends Component {
     };
     
     handleTextChange = (text) => {
-        console.log('change text ', text);
+        console.log('handle change text ', text);
     };
     
     handleSwapCurrency = () => {
-        console.log('press swap currency ');
+        console.log('handle swap currency');
+    };
+    
+    handleOptionPress = () => {
+        console.log('handle option press');
     };
 
   render() {
     return (
         <Container>
             <StatusBar translucent={false} barStyle="light-content" />
+            <Header 
+                onPress={this.handleOptionPress}
+            />
             <Logo />
             <InputWithButton
                 buttonText={TEMP_BASE_CURRENCY}
